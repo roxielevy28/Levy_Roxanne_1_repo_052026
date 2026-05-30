fae24bacdb70365f462a6ac5831c2842a784b33f
Roxanne Levy <roxielevy28@yahoo.com>
2026-05-30 17:31:08 -0400

    intial commit

---
+++ import requests.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get('https://books.toscrape.com/catalogue/set-me-free_988/index.html')
+soup = BeautifulSoup (page.content, 'html.parser')
+print(soup)
+
+headers = { "product_page_url, universal_ product_code (upc)"}
+
+
