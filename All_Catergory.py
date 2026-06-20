57d827d46cadf9cf92f8751f09f1ed2d4ace2fd0
roxielevy28 <roxielevy28@yahoo.com>
2026-06-20 18:28:46 -0400

    i got the link and name of category

--- All_Catergory.py
+++ All_Catergory.py
@@ -7,14 +7,18 @@ home_url= ('https://books.toscrape.com/index.html')
 page = requests.get(home_url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-category_links= []
-page_content= soup.find(id="default")
-category_list = soup.find(class_= 'nav nav-list')
-categories= category_list.find_all('a')
-print(categories)
+category_links = []
+page_content = soup.find(id="default")
+category_list = soup.find(class_='nav nav-list')
+categories = category_list.find_all('a')
+
 for category_url in categories:
-    relative_link = category_url['href']
-    complete_link = urljoin(home_url, relative_link)
-    category_links.append(complete_link)
+    catergory_name= category_url.text.strip()
+    link = category_url['href']
+    complete_link = urljoin(home_url, link)
+    category_links.append({
+        "name": catergory_name,
+        "url": complete_link
+    })
 print(category_links)
 
