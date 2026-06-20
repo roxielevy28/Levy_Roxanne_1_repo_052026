47c162d4205d24dd0547d27e7243703660414422
roxielevy28 <roxielevy28@yahoo.com>
2026-06-20 18:13:58 -0400

    that's it for today

--- Phase2_Young.Adult.category.py
+++ Phase2_Young.Adult.category.py
@@ -3,22 +3,26 @@ import requests
 from bs4 import BeautifulSoup
 
 category_url = "https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html"
-url_for_all_books= []
 
-while category_url:
-    page = requests.get(category_url)
-    soup = BeautifulSoup(page.text,'html.parser')
+url_for_all_books_in_category=[]
 
-    product = soup.find(id="default")
-    books_on_page= soup.find_all(class_= 'col-xs-6 col-sm-4 col-md-3 col-lg-3')
+page = requests.get(category_url)
+soup = BeautifulSoup(page.text,'html.parser')
 
-    for book_element in books_on_page:
+product = soup.find(id="default")
+books_on_page= soup.find_all(class_= 'col-xs-6 col-sm-4 col-md-3 col-lg-3')
+
+for book_element in books_on_page:
         link = book_element.find('h3').find('a')['href']
         complete_url = urljoin(category_url, link)
-        print(full_url)
+        url_for_all_books_in_category.append(complete_url)
+    # this retrieved the link for all books on the first page
+    
+print(url_for_all_books_in_category)
+
   # this works now. i got all the links on the first page
 
-    book_data = scrape_one_book(full_url)
+# book_data = scrape_one_book(complete_url)
     # *****you recommended call my book-scraper from my reusable scarpe. im not sure i understand this here******
     
 next_button = soup.find(class_='next')
@@ -30,7 +34,7 @@ else:
     category_url = None
 
 all_books = []
-for url in url_for_all_books:
+for url in url_for_all_books_in_category:
     book_data = scrape_one_book(url) # this is where i will call the function that i created in scrapetest.py.
     all_books.append(book_data)
 
