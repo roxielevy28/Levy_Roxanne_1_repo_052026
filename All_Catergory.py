c053196646ab45481c6e0424a038d3edda0f3527
roxielevy28 <roxielevy28@yahoo.com>
2026-06-20 17:29:46 -0400

    looks good

--- All_Catergory.py
+++ All_Catergory.py
@@ -3,11 +3,18 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-home_url= requests.get('https://books.toscrape.com/index.html')
+home_url= ('https://books.toscrape.com/index.html')
 page = requests.get(home_url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 category_links= []
 page_content= soup.find(id="default")
-category_list = page.find_all(class_= 'nav nav-list')
-print(category_list)
+category_list = soup.find(class_= 'nav nav-list')
+categories= category_list.find_all('a')
+print(categories)
+for category_url in categories:
+    relative_link = category_url['href']
+    complete_link = urljoin(home_url, relative_link)
+    category_links.append(complete_link)
+print(category_links)
+
