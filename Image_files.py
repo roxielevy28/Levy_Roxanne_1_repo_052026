55c82584aa2154006f9ef604b64071cf33baaa64
roxielevy28 <roxielevy28@yahoo.com>
2026-06-25 03:01:07 +0000

    save

--- Image_files.py
+++ Image_files.py
@@ -1,13 +1,11 @@
 from urllib.parse import urljoin
-import pandas as pd
 import requests
-import os
 from bs4 import BeautifulSoup
-from scrapetest import scrape_one_book
 
-home_url = 'https://books.toscrape.com/index.html'
-page = requests.get(home_url)
-soup = BeautifulSoup(page.content, 'html.parser')
+def get_all_category_links():
+    home_url = 'https://books.toscrape.com/index.html'
+    page = requests.get(home_url, timeout=10)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
 category_links = []
 page_content = soup.find(id="default")
@@ -22,28 +20,31 @@ for category_url in categories:
         "name": catergory_name,
         "url": complete_link
     })
-print(category_links)
+    return category_links
 
-for category in category_links:
-     cat_name = category["name"]
-     cat_url  = category["url"]
-     print(f"Scraping category: {cat_name}")
+def get_all_book_links ():
+    book_link: []
+    for book_element in books_on_page:
+            link = book_element.find("h3").find("a")["href"]
+            full_url = urljoin(cat_url, link)
+            all_book_urls.append(full_url)
 
-     all_book_urls = []
-     page = requests.get(cat_url)
-     soup = BeautifulSoup(page.text, 'html.parser')
-     books_on_page = soup.find_all(class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
-     all_book_urls = []
+all_book_urls = []
+page = requests.get(cat_url)
+soup = BeautifulSoup(page.text, 'html.parser')
+books_on_page = soup.find_all(class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
+all_book_urls = []
 
-     for book_element in books_on_page:
+for book_element in books_on_page:
          link = book_element.find('h3').find('a')['href']
          full_url = urljoin(cat_url, link)
          all_book_urls.append(full_url)
          
-     while True:
+while True:
         next_button = soup.find(class_="next")
         if not next_button:
             break
+
         next_page = next_button.find("a")["href"]
         cat_url = urljoin(cat_url, next_page)
         page = requests.get(cat_url)
@@ -53,12 +54,4 @@ for category in category_links:
             link = book_element.find("h3").find("a")["href"]
             full_url = urljoin(cat_url, link)
             all_book_urls.append(full_url)
-     all_books = []
-     for url in all_book_urls:
-        book_data = scrape_one_book(url)
-        all_books.append(book_data)
-     safe_name = cat_name.lower().replace(" ", "_")
-     df = pd.DataFrame(all_books)
-     df.to_csv(f"{safe_name}.csv", index=False)
-     print(f"Saved {len(all_books)} books to {safe_name}.csv")
-
+    
